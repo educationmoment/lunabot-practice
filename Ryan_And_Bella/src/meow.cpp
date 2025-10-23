440cca25a0294e52c59d3cc354a36e9e86f7f4f7
Annabella Bokla <144947840+annabella457@users.noreply.github.com>
2025-10-23 18:55:27 -0500

    Update meow.cpp

--- Ryan_And_Bella/src/meow.cpp
+++ Ryan_And_Bella/src/meow.cpp
@@ -1,1 +1,19 @@
 
+
+Class ControllerNode : public rclcpp::Node
+{
+  public:
+
+  ControllerNode(const std::string &can_interface)
+  : Node("controller_node"),
+    leftMotor(can_interface, LEFT_MOTOR),
+    rightMotor(can_interface, RIGHT_MOTOR)
+    {
+      RCLCPP_INFO(this.get_logger(), "beginning node")
+      RCLCPP_INFO(this.get_logger(), "configuring  motor controllers")
+
+      leftMotor.SetIdleMode(IdleMode::kBrake);
+  
+    
+
+}
