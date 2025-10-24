0fe48e486d27e7a9b87fb7c306a0e401a3763fdb
Annabella Bokla <144947840+annabella457@users.noreply.github.com>
2025-10-23 19:26:04 -0500

    Update vision.cpp

--- Ryan_And_Bella/src/vision.cpp
+++ Ryan_And_Bella/src/vision.cpp
@@ -7,15 +7,19 @@ class CameraNode : public rclcpp::Node
   CameraNode() : Node("Camera Node"), activeCameras({false})
   RCLCPP_INFO(this.get_logger(), "Camera loaded")
 
+  
+  if (cap_rgb1_.open(WEBCAM_ONE_PATH))
   {
-    if (cap_rgb1_.open(WEBCAM_ONE_PATH))
-    {
-      cap_rgb1_.set(cv::CAP_PROP_FPS, 30);
-      cap_rgb1_.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-      cap_rgb1_.set(cv::CAP_PROP_FRAME_LENGTH, 320);
-      this.activeCameras[Cameras::WEBCAM_ONE] = true;
-      RCLPP()_INFO(this.get_logger(), "webcam 1 is on");
-    }
-      
+    cap_rgb1_.set(cv::CAP_PROP_FPS, 30);
+    cap_rgb1_.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap_rgb1_.set(cv::CAP_PROP_FRAME_LENGTH, 320);
+    this.activeCameras[Cameras::WEBCAM_ONE] = true;
+    RCLPP()_INFO(this.get_logger(), "webcam 1 is on");
   }
+
+    
+      
+  private:
+
+  rclpp::Publisher<std_msgs::msgs:CompressedImage>SharedPtr rgb_can1_pub_;
 }
