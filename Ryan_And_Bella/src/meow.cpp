94042987d8751a102cd983ce8e623822400f0acc
Annabella Bokla <144947840+annabella457@users.noreply.github.com>
2025-10-23 19:03:31 -0500

    Update meow.cpp

--- Ryan_And_Bella/src/meow.cpp
+++ Ryan_And_Bella/src/meow.cpp
@@ -20,7 +20,28 @@ Class ControllerNode : public rclcpp::Node
       // finish configuring 
       // leftMotor.BurnFlash();
       // rightMotor.BurnFlash();
-  
-    
 
+    //drive train
+    {
+      float left_drive = 0.0
+      float right_drive = 0.0
+      float left_drive_raw = 0.0
+      float right_drive_raw = 0.0
+
+      float leftJS = joy_msg.axes(Gp::Axes::_LEFT_VERTICAL_STICK);
+      float rightJS = joy_msg.axes(Gp::Axes::_RIGHT_VERTICAL_STICK);
+
+      left_drive_raw = std::max(-1.0f, std::min(1.0f, leftJS));
+      right_drive_raw = std::max(-1.0f, std::min(1.0f, rightJS));
+
+      left_drive = computeStepOutput(left_drive_raw);
+      right_drive = computeStepOutput(right_drive_raw);
+
+      leftMotor.SetDutyCycle(left_drive);
+      rightMotor.SetDutyCycle(right_drive);
+    }  
+
+}
+
+int main(int argc, char **argv){
 }
