4857da4056343956c6a1a8c267bf8b1ae3134222
smehr83-sudo <smehr83@uic.edu>
2025-10-28 15:54:46 -0500

    creating variables for controller

--- Shradha.cpp
+++ Shradha.cpp
@@ -1,1 +1,66 @@
+#include "SparkMax.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/joy.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "interfaces_pkg/msg/motor_health.hpp"
+#include "interfaces_pkg/srv/depositing_request.hpp"
+#include "interfaces_pkg/srv/excavation_request.hpp"
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
 
+ELOCITY_MAX = 2500.0;  // rpm, after gearbox turns into 11.1 RPM
+const float VIBRATOR_OUTPUT = 1.0f; // Constant value for vibrator output
+
+enum CAN_IDs
+{
+  LEFT_MOTOR = 1,
+  RIGHT_MOTOR = 2,
+  LEFT_LIFT = 3,
+  RIGHT_LIFT = 4,
+  TILT = 5,
+  VIBRATOR = 6
+};
+
+namespace Gp
+{
+  enum Buttons
+  {
+    _A = 0,             // Excavation Autonomy
+    _B = 1,             // Stop Automation
+    _X = 2,             // Excavation Reset
+    _Y = 3,             // Deposit Autonomy
+    _LEFT_BUMPER = 4,   // Alternate between control modes
+    _RIGHT_BUMPER = 5,  // Vibration Toggle
+    _LEFT_TRIGGER = 6,  // Safety Trigger
+    _RIGHT_TRIGGER = 7, // Safety Trigger
+    _WINDOW_KEY = 8,    // Button 8 /** I do not know what else to call this key */
+    _D_PAD_UP = 12,     // Lift Actuator UP
+    _D_PAD_DOWN = 13,   // Lift Actuator DOWN
+    _D_PAD_LEFT = 14,   // Tilt Actuator Up   /** CHECK THESE */
+    _D_PAD_RIGHT = 15,  // Tilt Actuator Down /** CHECK THESE */
+    _X_BOX_KEY = 16
+  };
+
+  enum Axes
+  {
+    _LEFT_HORIZONTAL_STICK = 0,
+    _LEFT_VERTICAL_STICK = 1,
+    _RIGHT_HORIZONTAL_STICK = 2,
+    _RIGHT_VERTICAL_STICK = 3,
+  };
+}
+
+class ControllerNode : public rclcpp::Node
+{
+public:
+ControllerNode(const std::string &can_interface)
+    : Node("controller node"),
+        leftMotor(can_interface, LEFT_MOTOR),
+        rightMotor(can_interface, RIGHT_MOTOR),
+        leftLift(can_interface, LEFT_LIFT),
+        rightLift(can_interface, RIGHT_LIFT),
+        tilt(can_interface, TILT),
+        vibrator(can_interface, VIBRATOR),
+        vibrator_active()
