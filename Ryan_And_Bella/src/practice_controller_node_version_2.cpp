3c9161f7977b965aec6cbc87364cbf32a823524b
ryf269 <147371771+ryf269@users.noreply.github.com>
2025-11-11 19:34:05 -0600

    Update practice_controller_node_version_2.cpp

--- Ryan_And_Bella/src/practice_controller_node_version_2.cpp
+++ Ryan_And_Bella/src/practice_controller_node_version_2.cpp
@@ -1,4 +1,7 @@
 #include "SparkMax.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/joy.hpp"
+#include "std_msgs/msg/string.hpp"
 #include <cmath>
 #include <string>
 #include <cstdlib>
@@ -10,6 +13,8 @@ enum CAN_IDs  // assign ID to each motor
 {
   LEFT_MOTOR = 1,
   RIGHT_MOTOR = 2,
+  LEFT_LIFT = 3,
+  RIGHT_LIFT = 4,
 };
 
 namespace Controller // corresponding numbers are indicies
@@ -43,92 +48,128 @@ namespace Controller // corresponding numbers are indicies
 
 class ControllerNode: public rclcpp::Node  // ControllerNode inherits rclcpp::Node (ROS node) features
 {
-  private:
-    SparkMax leftMotor;   // controls the left motor
-    SparkMax rightMotor;  // controls the right motor
-
-    // joy_subscriber_ is of type rclcpp::Subscription<general_msgs::msg::Joy>::SharedPtr
-    // <general_msgs::msg::Joy> is the kind of message expected (messages of type Joy)
-    // this is basically a shared pointer to a subscription that receives Joy messages
-    rclcpp::Subscription<general_msgs::msg::Joy>::SharedPtr joy_subscriber_;
-
-    // rclcpp::Client<controller_pkg::srv::ExcavationRequest>::SharedPtr excavation_client_;  // 
-
-
   public:
-
-    // constructor
-    ControllerNode(const std::string &can_interface) : Node("controller_node")  // initialize rclcpp::Node (the base ROS node) with the name "controller_node"
+    ControllerNode(const std::string &can_interface)
+      : Node("controller_node"),
+        leftMotor(can_interface, LEFT_MOTOR),
+        rightMotor(can_interface, RIGHT_MOTOR),
+        leftLift(can_interface, LEFT_LIFT),
+        rightLift(can_interface, RIGHT_LIFT),
+        tilt(can_interface, TILT),
+        vibrator(can_interface, VIBRATOR),
+        vibrator_active_(false),
+        prev_vibrator_button_(false),
+        alternate_mode_active_(false),
+        prev_alternate_button_(false)
     {
-      // Initialize left and right motors
-      leftMotor = SparkMax(can_interface, LEFT_MOTOR);
-      rightMotor = SparkMax(can_interface, RIGHT_MOTOR);
-
-      // log start of new node creation
-      RCLCPP_INFO(this->get_logger(), "Beginning node...");
-      RCLCPP_INFO(this->get_logger(), "Configuring motor controllers...");
-
-      // set the motor types
-      leftMotor.SetMotorType(MotorType::kBrushless);
-      rightMotor.SetMotorType(MotorType::kBrushless);
-      // make the motors stay still
+      RCLCPP_INFO(this->get_logger(), "Begin Initializing Node");
+    
+      RCLCPP_INFO(this->get_logger(), "Initializing Motor Controllers");
+    
       leftMotor.SetIdleMode(IdleMode::kBrake);
       rightMotor.SetIdleMode(IdleMode::kBrake);
-      // configure other stuff
-
+      leftMotor.SetMotorType(MotorType::kBrushless);
+      rightMotor.SetMotorType(MotorType::kBrushless);
+      leftMotor.SetSensorType(SensorType::kHallSensor);
+      rightMotor.SetSensorType(SensorType::kHallSensor);
+      // Initializes the settings for the drivetrain motors
+    
+      leftLift.SetIdleMode(IdleMode::kBrake);
+      rightLift.SetIdleMode(IdleMode::kBrake);
+      leftLift.SetMotorType(MotorType::kBrushed);
+      rightLift.SetMotorType(MotorType::kBrushed);
+      leftLift.SetSensorType(SensorType::kEncoder);
+      rightLift.SetSensorType(SensorType::kEncoder);
+      // Initializes the settings for the lift actuators
+    
+      tilt.SetIdleMode(IdleMode::kBrake);
+      tilt.SetMotorType(MotorType::kBrushed);
+      tilt.SetSensorType(SensorType::kEncoder);
+      // Initializes the settings for the tilt actuator
+    
+      vibrator.SetIdleMode(IdleMode::kBrake);
+      vibrator.SetMotorType(MotorType::kBrushed);
+      vibrator.SetSensorType(SensorType::kEncoder);
+      // Initializes the settings fro the vibrator
+    
+      leftMotor.SetInverted(false);
+      rightMotor.SetInverted(true);
+      leftLift.SetInverted(true);
+      rightLift.SetInverted(true);
+      tilt.SetInverted(true);
+      vibrator.SetInverted(true);
+      // Initializes the inverting status
+    
+      leftMotor.SetP(0, 0.0002f);
+      leftMotor.SetI(0, 0.0f);
+      leftMotor.SetD(0, 0.0f);
+      leftMotor.SetF(0, 0.00021f);
+      // PID settings for left motor
+    
+      rightMotor.SetP(0, 0.0002f);
+      rightMotor.SetI(0, 0.0f);
+      rightMotor.SetD(0, 0.0f);
+      rightMotor.SetF(0, 0.00021f);
+      // PID settings for right motor
+    
+      leftLift.SetP(0, 1.51f);
+      leftLift.SetI(0, 0.0f);
+      leftLift.SetD(0, 0.0f);
+      leftLift.SetF(0, 0.00021f);
+      // PID settings for left lift
+    
+      rightLift.SetP(0, 1.51f);
+      rightLift.SetI(0, 0.0f);
+      rightLift.SetD(0, 0.0f);
+      rightLift.SetF(0, 0.00021f);
+      // PID settings for right lift
+    
+      // PID settings for tilt
+      tilt.SetP(0, 1.51f);
+      tilt.SetI(0, 0.0f);
+      tilt.SetD(0, 0.0f);
+      tilt.SetF(0, 0.00021f);
+      // PID settings for tilt
+    
       leftMotor.BurnFlash();
       rightMotor.BurnFlash();
+      leftLift.BurnFlash();
+      rightLift.BurnFlash();
+      tilt.BurnFlash();
+      vibrator.BurnFlash();
+      RCLCPP_INFO(this->get_logger(), "Motor Controllers Initialized");
+    
+      // ---ROS SUBSCRIPTIONS--- //
+      RCLCPP_INFO(this->get_logger(), "Initializing Joy Subscription");
+      joy_subscriber_ = this->create_subscription<sensor_msgs::msg::Joy>(
+          "/joy", 10,
+          std::bind(&ControllerNode::joy_callback, this, std::placeholders::_1));
+      RCLCPP_INFO(this->get_logger(), "Joy Subscription Initialized");
+    
+      health_subscriber_ = this->create_subscription<interfaces_pkg::msg::MotorHealth>(
+          "/health_topic", 10,
+          std::bind(&ControllerNode::position_callback, this, std::placeholders::_1));
+    
+      RCLCPP_INFO(this->get_logger(), "Initializing depositing, excavation, and travel client");
+      depositing_client_ = (this->create_client<interfaces_pkg::srv::DepositingRequest>("depositing_service"));
+      excavation_client_ = (this->create_client<interfaces_pkg::srv::ExcavationRequest>("excavation_service"));
+      RCLCPP_INFO(this->get_logger(), "Excavation, depositing clients initialized");
+    
+      RCLCPP_INFO(this->get_logger(), "Initializing Heartbeat Publisher");
+      heartbeatPub = this->create_publisher<std_msgs::msg::String>("/heartbeat", 10);
+      RCLCPP_INFO(this->get_logger(), "Heartbeat Publisher Initialized");
+    
+      RCLCPP_INFO(this->get_logger(), "Initializing Timer");
+      timer = this->create_wall_timer(
+          std::chrono::milliseconds(1000),
+          std::bind(&ControllerNode::publish_heartbeat, this));
+      RCLCPP_INFO(this->get_logger(), "Timer Initialized");
+    
+      RCLCPP_INFO(this->get_logger(), "Node Initialization Complete");
     }
 
-    // listen to joystick input by subscribing to joystick
-    joy_subscriber_ = this->create_subscription<general_msgs::msg::Joy>(       // joy_subscriber_ points to live ROS 2 subscription object 
-      "/joy", 10,                                                              // 10 is the queue size. Node can hold up to 10 messags
-      std::bind(&ControllerNode::joy_callback, this, std::placeholders::_1));  // call joy_callback function
-    RCLCPP_INFO(this->get_logger(), "Joystick subscriber initialized.");
-
-    // initialize excavation
-    // creates a service client inside the current node with 'create_client'
-    // <controller_pkg::srv::ExcavationRequest> defines the kind of request and response (the structure) messages this service will use
-    // "excavation_service" is the name of the service
-    // excavation_client_ = this->create_client<controller_pkg::srv::ExcavationRequest>("excavation_service");
-    // RCLCPP_INFO(this->get_logger(), "ControllerNode fully initialized.");
-
-    void joy_callback(const general_msgs::msg::Joy::SharedPtr joy_msg)
-    {
-      float left_drive = 0.0f;
-      float right_drive = 0.0f;
-      float left_drive_raw = 0.0f;
-      float right_drive_raw = 0.0f;
-
-      float leftJS = -(joy_msg.axes(Gp::Axes::_LEFT_VERTICAL_STICK));
-      float rightJS = -(joy_msg.axes(Gp::Axes::_RIGHT_VERTICAL_STICK));
-
-      left_drive_raw = std::max(-1.0f, std::min(1.0f, leftJS));
-      right_drive_raw = std::max(-1.0f, std::min(1.0f, rightJS));
-
-      leftMotor.SetVelocity(left_drive);
-      rightMotor.SetVelocity(right_drive);
-
-      RCLCPP_INFO_THROTTLE(
-        this->get_logger(), *this->get_clock(), 1000,
-        "Left Motor: %.2f | Right Motor: %.2f",
-        left_speed, right_speed);
-      
-      leftMotor.HeartBeat();
-      rightMotor.HeartBeat();
-    }
-
-    // send excavation request
-    /*
-    void send_excavation_request()
-    {
-      auto request = std::make_shared<controller_pkg::srv::ExcavationRequest::Request>();
-      request.start_excavation = true;
-      RCLCP_INFO(this->get_logger(), "sending excavation request...");
-
-      excavation_client_->async_send_request(request);
-    }
-    */
+  private:
+    
 };
 
 int main(int argc, char **argv){
