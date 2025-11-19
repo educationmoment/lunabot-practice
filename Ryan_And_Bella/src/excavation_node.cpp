bc36d1e4bb71394e86c6ccff99c181d7b17a6926
Annabella Bokla <144947840+annabella457@users.noreply.github.com>
2025-11-18 19:21:26 -0600

    Update excavation_node.cpp

--- Ryan_And_Bella/src/excavation_node.cpp
+++ Ryan_And_Bella/src/excavation_node.cpp
@@ -1,1 +1,56 @@
+#include "SparkMax.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "interfaces_pkg/srv/excavation_request.hpp"
+#include "interfaces_pkg/msg/motor_health.hpp"
 
+const float VIBRATOR_DUTY = 1.0f;
+const float ERROR = 0.1f;
+float buffer = 0.0f;
+
+SparkMax leftDrive("can0", 1);
+SparkMax rightDrive("can0", 2);
+SparkMax leftLift("can0", 3);
+SparkMax rightLift("can0", 4);
+SparkMax tilt("can0", 5);
+SparkMax vibrator("can0", 6); //Initalizes motor controllers
+
+rclcpp::Subscription<interfaces_pkg::msg::MotorHealth>::SharedPtr health_subscriber_;
+std::shared_ptr<rclcpp::Node> node;
+
+
+void MoveBucket (float lift_setpoint, float tilt_setpoint, bool activate_vibrator, float drive_speed) {
+    auto timer_start = std::chrono::high_resolution_clock::now();
+    bool leftLiftReached = (fabs(lift_setpoint - leftLift.GetPosition() ) <=  ERROR);
+    bool rightLiftReached = (fabs(lift_setpoint - rightLift.GetPosition() ) <=  ERROR);
+    bool tiltReached = (fabs(tilt_setpoint - tilt.GetPosition()) <=  ERROR);
+
+    while (!((leftLiftReached && rightLiftReached) && tiltReached)){
+        std::this_thread::sleep_for(std::chrono::milliseconds(5)); //prevents CAN buffer from overflowing
+        if (fabs(leftLift.GetPosition() - rightLift.GetPosition()) >= 0.2){
+            if (fabs(leftLift.GetPosition() - rightLift.GetPosition()) >= 0.75){
+                RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "WARNING: ACTUATORS GREATELY MISALIGNED");
+            }
+            leftLift.SetPosition(rightLift.GetPosition());
+            rightLift.SetPosition(rightLift.GetPosition());
+        } //block for lift realignment
+        else {
+            leftLift.SetPosition(lift_setpoint);
+            rightLift.SetPosition(lift_setpoint);
+            tilt.SetPosition(tilt_setpoint);
+        } //block for normal bucket movement
+
+        if (activate_vibrator) vibrator.SetDutyCycle(VIBRATOR_DUTY);
+        
+        leftDrive.SetVelocity(drive_speed);
+        rightDrive.SetVelocity(drive_speed);
+
+        if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - timer_start).count() > 5) {
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Skipping stage...");
+            break;
+        } //Timer for when to quit a stage due to timeout
+        
+        leftLiftReached = (fabs(lift_setpoint - leftLift.GetPosition() ) <=  ERROR);
+        rightLiftReached = (fabs(lift_setpoint - rightLift.GetPosition() ) <=  ERROR);
+        tiltReached = (fabs(tilt_setpoint - tilt.GetPosition() ) <=  ERROR); //Updates statuses
+    }
+}
