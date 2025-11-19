b556dff3d9c94066f9cc91badec6ec1e091ff564
ryf269 <147371771+ryf269@users.noreply.github.com>
2025-11-18 19:20:10 -0600

    Update practice_excavation_node.cpp

--- Ryan_And_Bella/src/practice_excavation_node.cpp
+++ Ryan_And_Bella/src/practice_excavation_node.cpp
@@ -1,2 +1,16 @@
 #include "SparkMax.hpp"
 #include "rclcpp/rclcpp.hpp"
+
+const float VIBRATOR_DUTY = 1.0f;
+const float ERROR = 0.1f;
+float buffer = 0.0f;
+
+SparkMax leftDrive("can0", 1);
+SparkMax rightDrive("can0", 2);
+SparkMax leftLift("can0", 3);
+SparkMax rightLift("can0", 4);
+SparkMax tilt("can0", 5);
+SparkMax vibrator("can0", 6); //Initalizes motor controllers
+
+rclcpp::Subscription<interfaces_pkg::msg::MotorHealth>::SharedPtr health_subscriber_;
+std::shared_ptr<rclcpp::Node> node;
