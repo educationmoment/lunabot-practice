9ab723532c27d9dec275128f7c630ca891ddba2b
ryf269 <147371771+ryf269@users.noreply.github.com>
2025-11-20 15:08:07 -0600

    11/20 update 1

--- Ryan_And_Bella/src/practice_excavation_node.cpp
+++ Ryan_And_Bella/src/practice_excavation_node.cpp
@@ -1,5 +1,7 @@
 #include "SparkMax.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "interfaces_pkg/srv/excavation_request.hpp"
+#include "interfaces_pkg/msg/motor_health.hpp"
 
 const float VIBRATOR_DUTY = 1.0f;
 const float ERROR = 0.1f;
@@ -14,3 +16,34 @@ SparkMax vibrator("can0", 6); //Initalizes motor controllers
 
 rclcpp::Subscription<interfaces_pkg::msg::MotorHealth>::SharedPtr health_subscriber_;
 std::shared_ptr<rclcpp::Node> node;
+
+void MoveBucket (float lift_setpoint, float tilt_setpoint, bool activate_vibrator, float drive_speed)
+{
+
+}
+
+void updateTiltPosition(const interfaces_pkg::msg::MotorHealth::SharedPtr health_msg){
+    buffer = health_msg->tilt_position;
+}
+
+void Excavate(const std::shared_ptr<interfaces_pkg::srv::ExcavationRequest::Request> request, std::shared_ptr<interfaces_pkg::srv::ExcavationRequest::Response> response)
+{
+    
+}
+
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv); 
+
+    node = rclcpp::Node::make_shared("excavation_node");
+
+    rclcpp::Service<interfaces_pkg::srv::ExcavationRequest>::SharedPtr service =
+    node->create_service<interfaces_pkg::srv::ExcavationRequest>("excavation_service", &Excavate);
+
+    health_subscriber_ = node->create_subscription<interfaces_pkg::msg::MotorHealth>(
+    "/health_topic", 10, updateTiltPosition);
+
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Excavation Initalized");
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+}
