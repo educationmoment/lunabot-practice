dd30ae29733d4b0428e6387bb7dd22a0710a46bc
smehr83-sudo <smehr83@uic.edu>
2025-12-11 16:27:25 -0600

    Update camerabox.cpp

--- Shradha/camerabox.cpp
+++ Shradha/camerabox.cpp
@@ -1,1 +1,6 @@
+<input type="file" accepts="image/*" capture="camera">
+<video> id="camera-feed" width="320" height="240" autoplay playsinline</video>
+<button id="capture-btn">Take Photo</button>
+<canvas id="camera-sensor" width="320" height="240" style="display::none;"></canvas
+<img id="photo-result" src="" alt="Captured Photo" style="display:none;">
 
